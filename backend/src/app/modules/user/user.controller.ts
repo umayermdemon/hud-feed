af4896d210b25615b83b76089fd73cb63dd984af
umayermdemon <mamudmdemon@gmail.com>
2025-09-20 07:47:36 +0600

    feat:add catchAsync function

--- backend/src/app/modules/user/user.controller.ts
+++ backend/src/app/modules/user/user.controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import catchAsync from "../utils/catchAsync";
 
-const createUserIntoDb = async (req: Request, res: Response) => {
+const createUserIntoDb = catchAsync(async (req, res) => {
   console.log(req, res);
-};
+});
 
 export const userController = { createUserIntoDb };
